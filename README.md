9ae3d1afb47c06e38098c14cd73ecb1cea83a37e
Alex Shabalin <shabalin.media@gmail.com>
2022-04-17 22:08:10 +0600

    Final version.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # etch-a-scetch
+
+Pet project Etch-a-Scetch (for practicing DOM-manipulations in JavaScript)
+
+Instructions I used from The Odin Project: 
+
+1. Create a webpage with a 16x16 grid of square divs.
+Create the divs using JavaScript. Don’t try making them by hand with copy and pasting in your HTML file!
+
+2. Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
+Hint: “Hovering” is what happens when your mouse enters a div and ends when your mouse leaves it. You can set up event listeners for either of those events as a starting point.
+
+3. Add a button to the top of the screen which will clear the current grid and send the user a popup asking for the number of squares per side for the new grid. Once entered, the new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad. Tip: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 let container = document.getElementsByClassName("container");
-let sizePerSize = 256;
-for(let x=0; x<sizePerSize; x++) {
+let boxPerSize = prompt('Welcome to Etch-a-Scetch! What size of brush do you want? Choose from 10 to 100.', 16);;
+let totalBoxes = boxPerSize ** 2;
+let widthHeigth = 500 / boxPerSize;
+
+// Create grid of blocks it the big box.
+for(let x=0; x<totalBoxes; x++) {
     let box = document.createElement('div');
+    box.style.width = `${widthHeigth}px`;
+    box.style.height = `${widthHeigth}px`;
+    box.style.backgroundColor = "#ffffff";
     box.className = "square";
     document.getElementById('box').appendChild(box);
 }
 
 let boxes = document.getElementsByClassName("square");
 
+// When mouseover, blocks changes their color.
 for (const box of boxes) {
   box.addEventListener('mouseover', function mouseOver() {
     box.style.backgroundColor = "#000000";
-    console.log('box clicked');
-  });
+  })
 }
 
 let button = document.createElement('button');
@@ -20,9 +27,15 @@ button.className = "clear";
 button.innerHTML = "Clear All";
 document.body.appendChild(button);
 
+// When button "Clear All" clicked, window reloads.
 button.onclick = function(){
     window.location.reload();
-    result = prompt('How many squares per side do you want? 100 - is max!', 16);    
-};
+}
+
+
+
+
+
+
 
 
